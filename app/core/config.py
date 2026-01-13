f1eeb4145ab318dba70245bdc2f6d22c83454485
Mal05x <138727970+Mal05x@users.noreply.github.com>
2026-01-13 21:42:02 +0100

    Update SECRET_KEY in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     POSTGRES_URL: str = "postgresql://postgres:postgres@db:5432/smartid"
-    SECRET_KEY: str = "your-secret-key"
+    SECRET_KEY: str = "302005"
     
     class Config:
         env_file = ".env"
